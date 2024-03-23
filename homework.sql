cc1f6546c2211d2a464eceda7d29ae6e999cd0fd
rohansinha01 <rohansinha01@gmail.com>
2024-03-23 17:23:38 -0400

    initial

---
+++ homework.sql
@@ -0,0 +1,16 @@
+-- Queries
+-- Some queries may require more than one command (i.e. you may need to get information about a team before you can complete a query for players). Test each command in PSQL to make sure it is correct.
+
+-- List the names of all NFL teams
+-- List the stadium name and head coach of all NFC teams
+-- List the head coaches of the AFC South
+-- The total number of players in the NFL
+
+-- The team names and head coaches of the NFC North and AFC East
+-- The 50 players with the highest salaries
+-- The average salary of all NFL players
+-- The names and positions of players with a salary above 10_000_000
+
+-- The player with the highest salary in the NFL
+-- The name and position of the first 100 players with the lowest salaries
+-- The average salary for a DE in the nfl
